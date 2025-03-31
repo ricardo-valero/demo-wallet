7630287c4e81ee2314f76627f08befbbe0312354
Ricardo Valero de la Rosa <55701657+ricardo-valero@users.noreply.github.com>
2025-03-31 00:20:42 -0600

    better nix workflow

--- flake.nix
+++ flake.nix
@@ -4,21 +4,53 @@
   };
   outputs = {nixpkgs, ...}: let
     systems = nixpkgs.lib.systems.flakeExposed;
+    mkNodejs = pkgs: nodejs: {
+      inherit nodejs;
+      corepack = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${nodejs}/bin/corepack enable --install-directory $out/bin
+      '';
+    };
+    mkShellApps = pkgs:
+      pkgs.lib.mapAttrs (
+        name: value:
+          if !(pkgs.lib.isDerivation value) && pkgs.lib.isAttrs value
+          then pkgs.writeShellApplication (value // {inherit name;})
+          else value
+      );
   in {
+    packages = nixpkgs.lib.genAttrs systems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodejs = mkNodejs pkgs pkgs.nodejs-slim_22;
+      in
+        mkShellApps pkgs {
+          install = {
+            runtimeInputs = builtins.attrValues nodejs;
+            text = ''
+              cd frontend
+              pnpm install
+            '';
+            meta.description = "Pnpm - install dependencies";
+          };
+          build = {
+            runtimeInputs = builtins.attrValues nodejs;
+            text = ''
+              cd frontend
+              pnpm vite build --base "''${BASE_PATH:-/}"
+            '';
+            meta.description = "Vite - build project";
+          };
+        }
+    );
     devShells = nixpkgs.lib.genAttrs systems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      nodejs = nodejs: {
-        inherit nodejs;
-        corepack = pkgs.runCommand "corepack-enable" {} ''
-          mkdir -p $out/bin
-          ${nodejs}/bin/corepack enable --install-directory $out/bin
-        '';
-      };
+      nodejs = mkNodejs pkgs pkgs.nodejs-slim_22;
     in {
       default = pkgs.mkShell {
         packages = builtins.attrValues (
           {inherit (pkgs) nixd nil alejandra;}
-          // nodejs pkgs.nodejs-slim_22
+          // nodejs
         );
       };
     });
